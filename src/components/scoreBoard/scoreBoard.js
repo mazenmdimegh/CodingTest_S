58ae3fe9356333a78b49a3345caa135af3d240f2
mazenmdimegh <mazenmdimagh@gmail.com>
2023-02-02 15:57:57 +0100

    ScoreBoard edit

---
+++ src/components/scoreBoard/scoreBoard.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+
+const scoreBoard = (props) => {
+    const { matchList } = props;
+
+    return (<div>
+        {matchList
+            .map((match) => {
+                return (
+                    <div key={match.id} className="scoreboard">
+                        <Game details={match}/>
+                    </div>
+                )
+            })}
+    </div>)
+}
