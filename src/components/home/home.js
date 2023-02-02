27a6eb73b5a8026593d3780f92e18d62d2818ca9
mazenmdimegh <mazenmdimagh@gmail.com>
2023-02-03 00:44:01 +0100

    scoreBoard update

---
+++ src/components/home/home.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+
+class Match {
+    constructor(homeTeam, awayTeam) {
+        this.id = v4();
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeTeamScore = 0;
+        this.awayTeamScore = 0;
+        this.status = "In Progress";
+        this.date = Date.now();
+    }
+}
+
+const home = () => {
+
+    return (
+        <div>
+            <s/>
+        </div>
+    )
+}
