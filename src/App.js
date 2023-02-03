899e289fe6aabe0b3973abf47924a14462a6c6c9
mazenmdimegh <mazenmdimagh@gmail.com>
2023-02-03 02:01:45 +0100

    AppJs edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/home/home"
 
 function App() {
   return (
     <div className="App">
-     <home/>
+      <Home />
     </div>
   );
 }
